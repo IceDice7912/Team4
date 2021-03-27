339cfffc80f882cc2799bf19653ec3e5ac75fa3d
IceDice <dice7912@naver.com>
2021-03-27 15:38:20 +0900

    4번째 공유 프로젝트 입니다.

---
+++ src/main/java/com/example/demo/Check.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class Check {
+
+	4번째 공유 프로젝트 입니다.
+	
+}
---
+++ src/main/java/com/example/demo/SpringBootGitTestLsm5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitTestLsm5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitTestLsm5Application.class, args);
+	}
+
+}
